6c5fabbc7a05da7e1493a163c7138354b1aa4b8c
Vanessa Santana <48105425+santanavanessa@users.noreply.github.com>
2025-09-26 20:02:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function showMenu() {
-    let mobileMenu = document.querySelector('.mobile-menu');
-    
-    if(mobileMenu.classList.contains('open')) {
-        mobileMenu.classList.remove('open');
-        document.querySelector('.icon').src = 'assets/menu_white.svg';
-    } else {
-        mobileMenu.classList.add('open');
-        document.querySelector('.icon').src = 'assets/close_white.svg';
-    }
+  let mobileMenu = document.querySelector(".mobile-menu");
+
+  if (mobileMenu.classList.contains("open")) {
+    mobileMenu.classList.remove("open");
+    document.querySelector(".icon").src = "assets/menu_white.svg";
+  } else {
+    mobileMenu.classList.add("open");
+    document.querySelector(".icon").src = "assets/close_white.svg";
+  }
 }
-
-
